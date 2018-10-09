8dbd1694d0343bdc5f139d313ebed7c29470a9b6
Nick Dickens <dickensn@fau.edu>
2018-10-08 21:42:29 -0400

    add appspec

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,12 +1,36 @@
 #!/usr/bin/env bash
 
-#TODO: is conda installed?
+user_dir="/home/ec2-user/.miniconda3"
 
-# check the environment
-result=`conda info --env | grep snakemake | cut -f 1 -d " "`
+#check if conda is already there
+keep_output=`conda info`
+status=$?
 
-if [[ $result -eq "snakemake" ]]; then
-  echo "snakemake environment already exists"
+if [[ $status -eq 1 ]]; then
+  curl -o miniconda3.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+  mkdir -p $user_dir
+  bash miniconda3.sh -f -b -p $user_dir
+  rm -f miniconda3.sh
+  chmod -R +x $user_dir/bin/
+  echo "PATH=\"$user_dir/bin:\$PATH\"" >> /home/ec2-user/.bashrc
+  sudo -u ec2-user conda config --add channels conda-forge
+  sudo -u ec2-user conda config --add channels defaults
+  sudo -u ec2-user conda config --add channels bioconda
+  sudo -u ec2-user conda install -y snakemake boto3
 else
-  conda create -n snakemake python=3 snakemake boto3 -y
+  # check snakemake is there
+  result=`conda list | grep snakemake | cut -f 1 -d " "`
+  if [[ $result -eq "snakemake" ]]; then
+    echo "snakemake environment already exists"
+  else
+    conda install -y snakemake python=3
+  fi
+
+  # check boto3
+  result=`conda list | grep boto3 | cut -f 1 -d " "`
+  if [[ $result -eq "boto3" ]]; then
+    echo "snakemake environment already exists"
+  else
+    conda install -y snakemake python=3
+  fi
 fi
