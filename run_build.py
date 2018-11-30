110709660e79b82b9fafd49accf2df1c87726904
Nick Dickens <dickensn@fau.edu>
2018-11-30 06:22:40 -0500

    update run_build

--- run_build.py
+++ run_build.py
@@ -16,20 +16,6 @@ task_version = 1
 if len(sys.argv) > 2:
     task_version = sys.argv[2]
 
-json_file = "test/test-{}.json".format(task_name)
-
-with open(json_file) as json_fh:
-    json_data = json.load(json_fh)
-
-memory = json_data.get("memory", None)
-threads = json_data.get("threads", None)
-
-if memory is None:
-    sys.exit("ERROR: json file {} does not contain a memory entry!".format(json_file))
-
-if threads is None:
-    sys.exit("ERROR: json file {} does not contain an entry for threads!".format(json_file))
-
 environment_variables= [
         {
         "name" : "task_name",
@@ -40,16 +26,6 @@ environment_variables= [
         "name" : "task_version",
         "value" : str(task_version),
         "type" : "PLAINTEXT"
-        },
-        {
-        "name" : "memory",
-        "value" : str(memory),
-        "type" : "PLAINTEXT"
-        },
-        {
-            "name" : "threads",
-            "value" : str(threads),
-            "type" : "PLAINTEXT"
         }
     ]
 
@@ -58,12 +34,7 @@ repo = Ecr(task_name)
 
 build = Codebuild("hboi-jobs-build-task", environment_variables)
 if build.success(max_time=25):
-    test_build = Codebuild("hboi-jobs-test-task", environment_variables)
-    if test_build.success(max_time=30):
-        pass
-    else:
-        print("Build failed for {}".format(test_build.get_name()))
+    print("Build ok for {}.".format(build.get_name()))
 else:
     print("Build failed for {}".format(build.get_name()))
 
-# retrieve batch submission and add job notification
